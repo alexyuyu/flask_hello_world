c25141c6285222ef8dd87210d75c592d3f7d64b1
alexyuyu <gqyu@alauda.io>
2017-05-03 16:49:31 +0800

    fix typo

--- webapp/app.py
+++ webapp/app.py
@@ -25,5 +25,5 @@ def log(msg):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    print('port:'+port)
+    print('port:{}'.format(port))
     app.run(host='0.0.0.0', port=port)
